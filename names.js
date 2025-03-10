09256eeb9d25e23540822ca56e71b2a165f89c97
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-10 17:37:55 +0100

    first commit

---
+++ names.js
@@ -0,0 +1,11 @@
+// Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+// Esporta la funzione dal file.
+
+function twoNames(firstName, lastName) {
+    return {
+        firstName: firstName,
+        lastName: lastName
+    };
+}
+
+console.log(twoNames("Tommaso", "Filippo"));
